68ea6a73a10b124182d5219151e7f2c51f3e8379
umeshbn <umeshbneelaguri.xworkz@gmail.com>
2021-12-27 22:23:12 +0530

    factorial series

---
+++ Factoseries.java
@@ -0,0 +1,13 @@
+class Factoseries{  
+ public static void main(String args[]){  
+  intfact=1;  
+  int n=100;   
+ 
+ for(int i=1;i<=n;i++){    
+      fact=fact*i;   
+System.out.println(fact);  
+ 
+  }  
+ 
+}
+}
